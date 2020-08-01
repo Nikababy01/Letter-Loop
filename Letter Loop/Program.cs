03f6bf4b07e3e7ac92fd86241b93e08d92217015
Monique.Bass01 <monique.bass01@gmail.com>
2020-08-01 15:18:37 -0500

    Add project files.

---
+++ Letter Loop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Letter_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("These are random scenarios");
+            string enteredLetters = "abcd";
+
+            string result = string.Empty;
+
+            for (var i = 0; i < enteredLetters.Length; i++)
+            {
+                char c = enteredLetters[i];
+
+                result += char.ToUpper(c);
+
+                result += new string(char.ToLower(c), i);
+
+                if (i < enteredLetters.Length - 1)
+                {
+                    result += "-";
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
